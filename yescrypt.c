bccaf8d6666987ac182457ef47f374668a96d2ef
WIDE888 <38092951+WIDE888@users.noreply.github.com>
2018-04-05 16:47:23 +0900

    Update yescrypt.c

--- yescrypt.c
+++ yescrypt.c
@@ -21,9 +21,9 @@
 #include "sha256.c"
 #include "yescrypt-best.c"
 
-#define YESCRYPT_N 2048
-#define YESCRYPT_R 8
-#define YESCRYPT_P 1
+#define YESCRYPT_N 4096
+#define YESCRYPT_R 16
+#define YESCRYPT_P 4
 #define YESCRYPT_T 0
 #define YESCRYPT_FLAGS (YESCRYPT_RW | YESCRYPT_PWXFORM)
 
