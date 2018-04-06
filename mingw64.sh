fdf5359a2e9e000de000f36c82fb7a013d9cdad5
WIDE888 <38092951+WIDE888@users.noreply.github.com>
2018-04-07 02:47:42 +0900

    Update mingw64.sh

--- mingw64.sh
+++ mingw64.sh
@@ -1,29 +1,8 @@
 ./autogen.sh
 
-CURL_PREFIX=/usr/local
-SSL_PREFIX=/usr/local/ssl
+LDFLAGS="-L depend/curl-7.38.0-devel-mingw64/lib64 -static" LIBCURL="-lcurldll" CFLAGS="-Ofast -march=native" ./configure --host=x86_64-w64-mingw32 --with-libcurl=depend/curl-7.38.0-devel-mingw64
 
-# gcc 4.4
-extracflags="-O3 -Wall -D_REENTRANT -fmerge-all-constants" # -funroll-loops -fvariable-expansion-in-unroller -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
-
-# gcc 4.8+
-# extracflags="$extracflags -Ofast -fuse-linker-plugin -ftree-loop-if-convert-stores" # -flto "
-
-# extracflags="-pg -static -fno-inline-small-functions"
-CFLAGS="-DCURL_STATICLIB -DOPENSSL_NO_ASM -DUSE_ASM $extracflags"
-# CPPFLAGS=""
-
-# icon
-windres res/icon.rc icon.o
-
-./configure --build=x86_64-w64-mingw32 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX \
-	CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" LDFLAGS="icon.o"
-
-make
+make -j 4
 
 strip -p --strip-debug --strip-unneeded cpuminer.exe
-
-if [ -e sign.sh ] ; then
-. sign.sh
-fi
 
