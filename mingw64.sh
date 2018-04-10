34e81e0d6d44d11e152b604c7821c209490b226f
WIDE888 <38092951+WIDE888@users.noreply.github.com>
2018-04-10 14:08:21 +0900

    Update mingw64.sh

--- mingw64.sh
+++ mingw64.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-make distclean || echo clean
-
-rm -f config.status
 ./autogen.sh || echo done
 
 LDFLAGS="-L depend/curl-7.38.0-devel-mingw64/lib64 -static" LIBCURL="-lcurldll" CFLAGS="-Ofast -march=native" ./configure --host=x86_64-w64-mingw32 --with-libcurl=depend/curl-7.38.0-devel-mingw64
